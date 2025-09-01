a038951378a6498faf28e008b195e502937b0a54
renatolinard <renatolinardjr@gmail.com>
2025-09-01 17:38:49 -0400

    corregir lsp nvim, alterar hypr com hyprpaper

---
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,8 @@
+-- ~/.config/nvim/lua/plugins/luasnip.lua
+return {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+}
