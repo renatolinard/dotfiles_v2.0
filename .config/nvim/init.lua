c462e88e38a01c9c85fe73d82cd863598e24e282
renatolinard <renatolinardjr@gmail.com>
2026-08-23 16:43:43 -0400

    mudanças para evitar erros na instalação com garuda

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -31,7 +31,6 @@ dofile(vim.g.base46_cache .. "statusline")
 
 require "options"
 require "autocmds"
-require "plugins"
 
 vim.schedule(function()
   require "mappings"
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,27 +1,6 @@
--- OPTIONS
-local set = vim.opt
-
 require "nvchad.options"
-vim.opt.guicursor = ""
 
-set.nu = true
-set.relativenumber = true
-set.colorcolumn = "80"
+-- add yours here!
 
--- search settings
-set.ignorecase = true
-set.smartcase = true
-
--- keep cursor at least 8 rows from top/bot
-set.scrolloff = 8
-
--- indentation and tabs
-set.tabstop = 4
-set.shiftwidth = 4
-set.autoindent = true
-set.expandtab = true
-
--- wrap column 80
-set.textwidth = 80
-set.wrap = true
-
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
